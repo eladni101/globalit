20867147c84197679431120b1967048c73f80d0a
eladni101 <eladnisan10@gmail.com>
2018-08-11 21:12:47 +0300

    login start

--- EladFront/src/app/login/login-module/login.module.ts
+++ EladFront/src/app/login/login-module/login.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginHomeViewComponent } from '../../login-home-view/login-home-view.component';
-import { LoginForgotViewComponent } from '../../login-forgot-view/login-forgot-view.component';
+import { LoginHomeViewComponent } from '../login-home-view/login-home-view.component';
+import { LoginForgotViewComponent } from '../login-forgot-view/login-forgot-view.component';
 
 @NgModule({
   imports: [
